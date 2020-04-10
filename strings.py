f7e922cfb1761fe499fb1d1982dd40a2cb0b754f
Brad Chandonnet <bchandonnet@partnercomm.net>
2020-04-10 13:20:40 -0400

    Adding our first few files

---
+++ strings.py
@@ -0,0 +1,11 @@
+first_name = "Caleb"
+last_name = " Chandonnet"
+full_name = first_name + last_name
+print(first_name)
+
+age = 12
+print(age)
+
+print("Hello, my name is %s. I am %i years old." % (full_name, age))
+
+  
