d00eb7a5dbf0fa573fadb465e51f01cf504ab094
Brad Chandonnet <bchandonnet@partnercomm.net>
2020-04-12 12:33:35 -0400

    list program

--- list.py
+++ list.py
@@ -1,22 +1,4 @@
-#int1 = 2+1
-#int2 = 2+1
-#int3 = int1 + int2
-#print(int3)
-
-#n = 1
-#while n <= 3:
-#    answer = input()
-#    print(str(n) + ". " + answer)
-#    n += 1
-
-#item1 = input("Add an item: ")
-#item2 = input("Add an item: ")
-#item3 = input("Add an item: ")
-#print(" ")
-#print("HERE IS YOUR LIST")
-#print("1. " + item1)
-#print("2. " + item2)
-#print("3. " + item3)
+#This program makes a simple list
 
 list_items = []
 add_item = 'y'
@@ -34,6 +16,3 @@ if(add_item != 'y'): #Show the list
     for i in list_items: 
         print(str(item_number) + ". " + i)
         item_number += 1
-
-
-
