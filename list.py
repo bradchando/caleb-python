5064dffd7201aac0ccdb9c2d4f2e1c171b83715f
Brad Chandonnet <bchandonnet@partnercomm.net>
2020-04-29 19:51:00 -0400

    program work

--- list.py
+++ list.py
@@ -1,18 +1,30 @@
 #This program makes a simple list
-
-list_items = []
-add_item = 'y'
-name = input("What is your name? ")
-item_number = 1 
-
-while(add_item == 'y'):    
-    list_items.append(input('Item to add: '))
-    add_item = input("Would you like to add another item? ")
+":def password_check(password):
+    if password == "Caleb
+        return True
+    else:
+        return False
     
-if(add_item != 'y'): #Show the list
-    print('------------------------')
-    print(name + "'s List")
+password = input("enter the password: ")
+
+if password_check(password):
+
+    list_items = []
+    add_item = 'y'
+    name = input("What is your name? ")
+    item_number = 1 
     
-    for i in list_items: 
-        print(str(item_number) + ". " + i)
-        item_number += 1
+    while(add_item == 'y'):    
+        list_items.append(input('Item to add: '))
+        add_item = input("Would you like to add another item? ")
+        
+    if(add_item != 'y'): #Show the list
+        print('------------------------')
+        print(name + "'s List")
+        
+        for item in list_items: 
+            print(str(item_number) + ". " + item)
+            item_number += 1
+
+else:
+    print("sorry, wrong password")
